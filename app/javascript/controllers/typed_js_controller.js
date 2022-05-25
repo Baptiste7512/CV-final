15cf3849f0e56fb118288e84dbc0ec0c2f0da77d
baptiste vigneron <vigneron.baptiste@live.fr>
2022-05-25 17:40:05 +0200

    deployment warning

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -2,6 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    console.log("hello from typed_controller!")
+    console.log("hello from typed _controller!")
   }
 }
